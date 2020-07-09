e8ed42fae08951dc7799096f6cdf03038d62c61b
kenta-matsuzaki <kenta6910@gmail.com>
2020-07-09 14:53:50 +0900

    Main.javaにお化けキノコのインスタンスを作成し、冒険を開始

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 public class Main {
   public static void main(String[] args){
-    // 1.勇者を生成
+    // 1.勇者のインスタンスを生成
     Hero h = new Hero();
     // 2.フィールドに初期値をセット
     h.name = "ケンタ";
     h.hp = 23;
     System.out.println("勇者" + h.name + "を生み出した！");
-    // 3.勇者のメソッドを呼び出す
-    h.sit(5);
+
+    // お化けキノコのインスタンスの生成
+    Matango m1 = new Matango();
+    m1.hp = 13;
+    m1.suffix = 'A';
+    Matango m2 = new Matango();
+    m2.hp = 14;
+    m2.suffix = 'B';
+
+    // 冒険の始まり
     h.slip();
-    h.sit(25);
+    m1.run();
+    m2.run();
     h.run();
   }
 }
