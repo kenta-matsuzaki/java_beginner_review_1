62debf8a0fabecdf0a68b4ead4fd56373553e917
kenta-matsuzaki <kenta6910@gmail.com>
2020-07-09 15:20:13 +0900

    Clericクラスのインスタンスを生成し、selfAid(),pray()メソッドを発動

--- Main.java
+++ Main.java
@@ -11,12 +11,24 @@ public class Main {
     Matango m1 = new Matango();
     m1.hp = 13;
     m1.suffix = 'A';
+    System.out.println("お化けキノコ" + m1.suffix + "が現れた！");
     Matango m2 = new Matango();
     m2.hp = 14;
     m2.suffix = 'B';
+    System.out.println("お化けキノコ" + m2.suffix + "が現れた！");
+
+    // 聖職者のインスタンスの作成
+    Cleric c = new Cleric();
+    c.name = "美穂子";
+    System.out.println("聖職者" + c.name + "を生み出した！");
 
     // 冒険の始まり
     h.slip();
+    c.selfAid();
+    c.selfAid();
+    c.selfAid();
+    c.selfAid();
+    c.pray(6);
     m1.run();
     m2.run();
     h.run();
