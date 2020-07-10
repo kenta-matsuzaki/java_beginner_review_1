f0b3414c09f788a52a0e15ebd8505ea8258d1d85
kenta-matsuzaki <kenta6910@gmail.com>
2020-07-10 14:06:12 +0900

    勇者が装備する武器のフィールドを追加

--- Hero.java
+++ Hero.java
@@ -1,6 +1,12 @@
 public class Hero {
   String name;
   int hp;
+  Sword sword;
+
+  void attack(){
+    System.out.println(this.name + "は攻撃した！");
+    System.out.println("的に5ポイントのダメージを与えた！");
+  }
 
   void sleep(){
     this.hp = 100;
