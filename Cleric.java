9d0e55085d8c6821504e63301752f40a14635345
kenta-matsuzaki <kenta6910@gmail.com>
2020-07-10 14:57:12 +0900

    コンストラクタの追加。

--- Cleric.java
+++ Cleric.java
@@ -2,14 +2,28 @@ import java.util.Random;
 
 public class Cleric {
   String name;
-  final int MAXHP = 20;
+  static final int MAXHP = 20;
   int hp = 20;
-  final int MAXMP = 50;
+  static final int MAXMP = 50;
   int mp = 50;
+
+  public Cleric(String name, int hp, int mp){
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+  }
+
+  public Cleric(String name, int hp){
+    this(name, hp, Cleric.MAXMP);
+  }
+
+  public Cleric(String name){
+    this(name, Cleric.MAXHP, Cleric.MAXMP);
+  }
 
   void selfAid(){
     this.mp -= 5;
-    this.hp = this.MAXHP;
+    this.hp = Cleric.MAXHP;
     System.err.println(this.name + "はセルフエイドを唱えた！");
     System.out.println(this.name + "のHPが最大まで回復した！");
   }
@@ -19,7 +33,7 @@ public class Cleric {
     // 論理上の回復量を乱数を用いて決定する
     int recover = new Random().nextInt(3) + sec;
     // 実際の回復量を計算する
-    int recoverActual = Math.min(this.MAXMP - this.mp, recover);
+    int recoverActual = Math.min(Cleric.MAXMP - this.mp, recover);
 
     this.mp += recoverActual;
     System.out.println(this.name + "のMPが" + recoverActual + "回復した！");
