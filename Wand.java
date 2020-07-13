0327337aa9c93a7a3e734e0c7e4b0303aea7aa70
kenta-matsuzaki <kenta6910@gmail.com>
2020-07-13 14:14:52 +0900

    Wand(杖)クラスを作成し、ゲッター・セッターを追加

---
+++ Wand.java
@@ -0,0 +1,20 @@
+public class Wand {
+  private String name;
+  private double power;
+
+  // nameのゲッターとセッター
+  public String getName(){
+    return this.name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+
+  // powerのゲッターとセッター
+  public double getPower(){
+    return this.power;
+  }
+  public void setPower(double power){
+    this.power = power;
+  }
+}
