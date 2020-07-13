7339a3f425c5301e88815ee8827c1f8a6be337f5
kenta-matsuzaki <kenta6910@gmail.com>
2020-07-13 14:15:14 +0900

    全てのフィールドをprivateに設定し、ゲッター・セッターを追加

--- Wizard.java
+++ Wizard.java
@@ -1,9 +1,46 @@
 public class Wizard {
-  String name;
-  int hp;
-  
+  private int hp;
+  private int mp;
+  private String name;
+  private Wand wand;
+
+  // hpのゲッターとセッター
+  public int getHp(){
+    return this.hp;
+  }
+  public void setHp(int hp){
+    this.hp = hp;
+  }
+
+  // mpのゲッターとセッター
+  public int getMp(){
+    return this.mp;
+  }
+  public void setMp(int mp){
+    this.mp = mp;
+  }
+
+  // nameのゲッターとセッター
+  public String getName(){
+    return this.name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+
+  // wandのゲッターとセッター
+  public Wand getWand(){
+    return this.wand;
+  }
+  public void setWand(Wand wand){
+    this.wand = wand;
+  }
+
   void heal(Hero h){
-    h.setHp(h.getHp() + 10);
-    System.out.println(this.name + "はhealを唱えた！" + h.getName() + "のHPを10回復した！");
+    int basePoint = 10;
+    int recovPoint = (int) (basePoint * this.wand.getPower());
+
+    h.setHp(h.getHp() + recovPoint);
+    System.out.println(this.name + "はhealを唱えた！" + h.getName() + "のHPを" + recovPoint + "回復した！");
   }
 }
