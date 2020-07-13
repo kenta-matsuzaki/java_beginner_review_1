406cc22e93ae4030113cbb5b5ad9ff087ac7ba6d
kenta-matsuzaki <kenta6910@gmail.com>
2020-07-13 14:37:27 +0900

    各セッターに異常値を弾く記述を追加

--- Wizard.java
+++ Wizard.java
@@ -5,34 +5,39 @@ public class Wizard {
   private Wand wand;
 
   // hpのゲッターとセッター
-  public int getHp(){
-    return this.hp;
-  }
+  public int getHp(){ return this.hp; }
   public void setHp(int hp){
-    this.hp = hp;
+    if(hp < 0){
+      this.hp = 0;
+    } else {
+      this.hp = hp;
+    }
   }
 
   // mpのゲッターとセッター
-  public int getMp(){
-    return this.mp;
-  }
+  public int getMp(){ return this.mp; }
   public void setMp(int mp){
+    if(mp < 0){
+      throw new IllegalArgumentException("MPが不正な値です。処理を中断します。");
+    }
     this.mp = mp;
   }
 
   // nameのゲッターとセッター
-  public String getName(){
-    return this.name;
-  }
+  public String getName(){ return this.name; }
   public void setName(String name){
+    if(name == null || name.length() < 3){
+      throw new IllegalArgumentException("魔法使いに設定されようとしている名前が異常です。処理を中断します。");
+    }
     this.name = name;
   }
 
   // wandのゲッターとセッター
-  public Wand getWand(){
-    return this.wand;
-  }
+  public Wand getWand(){ return this.wand; }
   public void setWand(Wand wand){
+    if(wand == null){
+      throw new IllegalArgumentException("設定されようとしている杖がnullです");
+    }
     this.wand = wand;
   }
 
